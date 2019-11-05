f13e7cf84f6f26f1a6c19edc89b40743508eb0bc
joaaofiilho <joaot10112@edu.unifor.br>
2019-11-05 19:36:56 -0300

    Adicionada frases do justin bieber Preparado o utils de celebridades com a lista de celebridades

--- app/src/main/java/com/joaaoferreiira/celebridades/database/entity/CelebridadeEntity.kt
+++ app/src/main/java/com/joaaoferreiira/celebridades/database/entity/CelebridadeEntity.kt
@@ -2,11 +2,10 @@ package com.joaaoferreiira.celebridades.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.Relation
 
 @Entity(tableName = "celebridadeEntity")
 data class CelebridadeEntity(
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey
     val id: Int,
     var nome: String,
     var imagem: Int)
--- app/src/main/java/com/joaaoferreiira/celebridades/util/CelebridadesUtil.kt
+++ app/src/main/java/com/joaaoferreiira/celebridades/util/CelebridadesUtil.kt
@@ -6,16 +6,16 @@ import com.joaaoferreiira.celebridades.database.entity.CelebridadeEntity
 object CelebridadesUtil {
     fun getCelebridades(): List<CelebridadeEntity> {
         return listOf(
-            CelebridadeEntity(0, "Ana Paula Padrão", R.drawable.ana_paula_padrao),
-            CelebridadeEntity(0, "Beyonce", R.drawable.beyonce),
-            CelebridadeEntity(0, "Daniel Radcliffe", R.drawable.daniel_radcliffe),
-            CelebridadeEntity(0, "Ellen DeGeneres", R.drawable.ellen_degeneres),
-            CelebridadeEntity(0, "Emma Watson", R.drawable.emma_watson),
-            CelebridadeEntity(0, "Fatima Bernardes", R.drawable.fatima_bernardes),
-            CelebridadeEntity(0, "Gloria Maria", R.drawable.gloria_maria),
-            CelebridadeEntity(0, "Paola Carosella", R.drawable.paola_carosella),
-            CelebridadeEntity(0, "Taylor Swift", R.drawable.taylor_swift),
-            CelebridadeEntity(0, "William Bonner", R.drawable.william_bonner)
+            CelebridadeEntity(1, "Justin Bieber", R.drawable.justin_bieber),
+            CelebridadeEntity(2, "Beyonce", R.drawable.beyonce),
+            CelebridadeEntity(3, "Daniel Radcliffe", R.drawable.daniel_radcliffe),
+            CelebridadeEntity(4, "Ellen DeGeneres", R.drawable.ellen_degeneres),
+            CelebridadeEntity(5, "Emma Watson", R.drawable.emma_watson),
+            CelebridadeEntity(6, "Fatima Bernardes", R.drawable.fatima_bernardes),
+            CelebridadeEntity(7, "Gloria Maria", R.drawable.gloria_maria),
+            CelebridadeEntity(8, "Paola Carosella", R.drawable.paola_carosella),
+            CelebridadeEntity(9, "Taylor Swift", R.drawable.taylor_swift),
+            CelebridadeEntity(10, "William Bonner", R.drawable.william_bonner)
         )
     }
 }
