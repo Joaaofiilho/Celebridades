1afce0a027ab72c34edff8915d9bef6bffde6e6e
joaaofiilho <joaot10112@edu.unifor.br>
2019-11-06 00:21:09 -0300

    Adicionada frases de 8 celebridades

--- app/src/main/java/com/joaaoferreiira/celebridades/util/CelebridadesUtil.kt
+++ app/src/main/java/com/joaaoferreiira/celebridades/util/CelebridadesUtil.kt
@@ -10,12 +10,15 @@ object CelebridadesUtil {
             CelebridadeEntity(2, "Beyonce", R.drawable.beyonce),
             CelebridadeEntity(3, "Daniel Radcliffe", R.drawable.daniel_radcliffe),
             CelebridadeEntity(4, "Ellen DeGeneres", R.drawable.ellen_degeneres),
-            CelebridadeEntity(5, "Emma Watson", R.drawable.emma_watson),
             CelebridadeEntity(6, "Fatima Bernardes", R.drawable.fatima_bernardes),
             CelebridadeEntity(7, "Gloria Maria", R.drawable.gloria_maria),
-            CelebridadeEntity(8, "Paola Carosella", R.drawable.paola_carosella),
-            CelebridadeEntity(9, "Taylor Swift", R.drawable.taylor_swift),
-            CelebridadeEntity(10, "William Bonner", R.drawable.william_bonner)
+            CelebridadeEntity(14, "Emma Watson", R.drawable.emma_watson),
+            CelebridadeEntity(15, "Chico Buarque", R.drawable.chico_buarque),
+            CelebridadeEntity(16, "Lulu Santos", R.drawable.lulu_santos),
+            CelebridadeEntity(17, "Marília Mendonça", R.drawable.marilia_mendonca),
+            CelebridadeEntity(18, "Lady Gaga", R.drawable.lady_gaga),
+            CelebridadeEntity(19, "Will Smith", R.drawable.will_smith),
+            CelebridadeEntity(20, "Taylor Swift", R.drawable.taylor_swift)
         )
     }
 }
